c93f604dddc1b97c02d0d181f7209035581ae3f8
lapan13m <ariirwandi13@gmail.com>
2020-03-15 19:17:45 +0700

    add all file to repo

---
+++ Assets/Scripts/BackToMateri.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMateri : MonoBehaviour
+{
+    void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                BackButton();
+            }
+        }
+
+        public void BackButton(){
+        	UnityEngine.SceneManagement.SceneManager.LoadScene("Materi");
+        }
+}
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+    public void LoadScene(string scenename) {
+    	SceneManager.LoadScene(scenename);
+    }
+
+    public void ExitApp() {
+         Application.Quit();
+    }
+
+    public void OpenMyUrl() {
+    	Application.OpenURL("https://drive.google.com/file/d/1GLA34FCQiYZ4jBBW_EAM87iOtvQvKVXC/view?usp=sharing");
+    }
+}
